32e4a8ffb5c5e57343f362d341bca79a141e2c73
yasunaga shuto <yasunaga.shuto@gmail.com>
2023-03-08 18:06:25 +0900

    紹介文変更

--- locales/en.ts
+++ locales/en.ts
@@ -8,7 +8,7 @@ export default {
   INTRODUCTION_TEXT: "Introduction",
   INTRODUCTION: `Hi!
     My name is Yasunaga working as engineer in Tokyo.<br />
-    When I was in university in 2013, I first experienced programming(Java) in class and I thought it was fun.
+    When I was in university in 2013, I first learned programming(Java) in class and created some games.
     In April 2017, I started working as full stack engineer and continue working until now,
     leading engineering in some companies.
     I can code frontend, backend and infrastructure. I'm good at writing backend, designing database structure and so on.<br />
