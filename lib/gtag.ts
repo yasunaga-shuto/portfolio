27047ec2eac8ba18f8a0be5de6b1741518f17d32
yasunaga shuto <yasunaga.shuto@gmail.com>
2025-09-01 11:54:59 +0900

    修正

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,7 +2,7 @@ export const GA_TRACKING_ID = process.env.GA_TRACKING_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-  // @ts-ignore
+  // @ts-expect-error gtag設置のため
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   })
@@ -10,7 +10,7 @@ export const pageview = (url: string) => {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: { action: string, category: string, label: string, value: string }) => {
-  // @ts-ignore
+  // @ts-expect-error　gtag設置のため
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
